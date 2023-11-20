b9f685873d272bab2288d271a119377483f17e97
codepnw <idchokms01@outlook.com>
2023-11-20 22:22:38 +0700

    update and delete fact

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/codepnw/articles/database"
+	"github.com/codepnw/articles/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
@@ -19,6 +20,7 @@ func main() {
 	setupRoutes(app)
 
 	app.Static("/", "./public")
+	app.Use(handlers.NotFound)
 
 	app.Listen(":3000")
 }
